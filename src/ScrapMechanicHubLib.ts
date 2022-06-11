4550004616a9ee24c16ded57455e60e05364f64f
Luncert <2725115515@qq.com>
2022-06-11 17:55:36 +0800

    .

--- src/ScrapMechanicHubLib.ts
+++ src/ScrapMechanicHubLib.ts
@@ -15,16 +15,28 @@ const DATA_DIR = join(__dirname, 'data')
 export class ScrapMechanicHub {
 
   // private db: Low
+  private gamePath: string
   private contentDir: string
 
-  constructor(private gamePath: string) {
+  constructor() {
     // this.db = new LowWithLodash(new JSONFile(join(DATA_DIR, 'db.json')))
-    this.contentDir = join(gamePath, '../../workshop/content/387990/')
   }
 
-  loadContent(contentId: string) {
-    let contentPath = join(this.contentDir, contentId)
-    let content = loadContent(contentPath)
-    console.log(content)
+  public loadGame(gamePath: string, logger: ActionLogger) {
+    this.gamePath = gamePath
+    this.contentDir = join(gamePath, '../../workshop/content/387990/')
+    logger.write('hello')
+    setTimeout(() => 
+    logger.close(true), 1000)
   }
+
+  public loadContent(contentId: string) {
+    let contentPath = join(this.contentDir, contentId)
+    return loadContent(contentPath)
+  }
+}
+
+export interface ActionLogger {
+  write(data: string): void
+  close(actionSuccess: boolean): void
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { ScrapMechanicHub } from './ScrapMechanicHubLib'
 
-let hub = new ScrapMechanicHub('F:\\SteamLibrary\\steamapps\\common\\Scrap Mechanic')
+let hub = new ScrapMechanicHub()
+hub.loadGame('F:\\SteamLibrary\\steamapps\\common\\Scrap Mechanic', null)
 hub.loadContent('2815418786')
